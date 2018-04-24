2cbbaadf23d0799ca8379f44481f0951550dd784
anddroidty <389170487@qq.com>
2018-04-24 14:50:53 +0800

    first

---
+++ wx/pages/statistical/statistical.js
@@ -0,0 +1,111 @@
+// pages/statistical/statistical.js
+import * as echarts from '../../ec-canvas/echarts'
+const app = getApp();
+
+function initChart(canvas, width, height) {
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height
+  });
+  canvas.setChart(chart);
+  var option = {
+
+    backgrondColor: "#2e2d3d",
+    color: ["#FF0000", "#0FFF00"],
+    series: [{
+      label: {
+        normal: {
+          fontSize: 14
+        }
+      },
+
+      type: 'pie',
+      center: ['50%', '50%'], radius: [0, '40%'],
+
+      data: [
+        {
+          value: 78,
+          name: '已打卡人数'
+        }, {
+          value: 22,
+          name: '未打卡人数'
+        }
+      ],
+      itemStyle: {
+        emphasis: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 2, 2, 0.3)'
+        }
+      }
+    }]
+  };
+
+  chart.setOption(option);
+  return chart;
+}
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    ec: { onInit: initChart }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
